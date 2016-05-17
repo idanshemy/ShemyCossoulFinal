834cd1fc876acaaaf71084c388fd52b75e09bab3
Idan Shemy <shemy.idan@gmail.com>
2016-05-17 07:47:30 -0700

    Added a Display class tempalte. Created EndPoint object. Implemented methods in Grid.

---
+++ src/Display.java
@@ -0,0 +1,72 @@
+import java.util.LinkedHashMap;
+import java.util.Map;
+
+import processing.core.PApplet;
+import processing.core.PImage;
+//THIS ONE IS OURS!!!!!!!!!
+public class Display {
+	
+	
+	// Colors used for empty locations.
+	private static final int EMPTY_COLOR = 0xFFFFFFFF;
+
+	private PApplet p; // the applet we want to display on
+
+	private int x, y, w, h; // (x, y) of upper left corner of display
+	// the width and height of the display
+
+	private float dx, dy; // calculate the width and height of each box
+	// in the field display using the size of the field
+	// and the width and height of the display
+
+	private int rows, cols;
+
+	// A map for storing colors for participants in the simulation
+	private Map<Object, Integer> colors;
+	private Map<Object, PImage> images;
+
+	public Display(PApplet p, int x, int y, int w, int h) {
+		this.x = x;
+		this.y = y;
+		this.w = w;
+		this.h = h;
+		this.p = p;
+
+		colors = new LinkedHashMap<Object, Integer>();
+		images = new LinkedHashMap<Object, PImage>();
+	}
+
+	public void drawGrid(int[][] f) {
+		int piece;
+
+		int numcols = f[0].length;
+		int numrows = f.length;
+
+		for (int i = 0; i < numrows; i++) {
+			for (int j = 0; j < numcols; j++) {
+				int gridState = f[i][j];
+				int gridColor = 0;
+
+				if (gridState == Grid.NOT_VISITED) {
+					gridColor = this.EMPTY_COLOR;
+				} else {
+					gridColor = getColor(gridState);
+				}
+
+				p.fill(gridColor);
+				p.rect(x + j * dx, y + i * dy, dx, dy);
+
+			}
+		}
+	}
+	
+	private Integer getColor(int pieceType) {
+		Integer col = colors.get(pieceType);
+		
+		if (col == null) { // no color defined for this class
+			return Grid.NOT_VISITED;
+		} else {
+			return col;
+		}
+	}
+}
